9a9a0b07306c92ec8a823416f91483172129a0b5
yupitsmeeric <muhammadilhamfadhilah@gmail.com>
2023-03-08 21:51:28 +0800

    naerstneiran

---
+++ CSV2ROM.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+from math import log, ceil
+import re
+
+
+# In[12]:
+
+
+data = pd.read_csv("./rom.csv")
+tits = data.columns
+
+
+# In[13]:
+
+
+file_loader = FileSystemLoader('.')
+env = Environment(loader=file_loader)
+template = env.get_template('template.luc')
+output = template.render(titles=tits, data = data, 
+                         reversed = reversed, re=re, log = log,
+                         len = len, ceil=ceil)
+print(output)
+
+
+# In[14]:
+
+
+
+with open('testRom.luc', 'w') as file:
+    file.write(output)
+
+
+# In[10]:
+
+
+
+with open('testRom.luc', 'w') as file:
+    file.write("lolllll")
+
